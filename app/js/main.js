7aa52da368fff32552ab97f4a457c50e6b3355a2
ozinkevych <zinkevicsasa1@gmail.com>
2024-06-05 17:30:43 +0300

    task 3 - made a share instantly block

--- app/js/main.js
+++ app/js/main.js
@@ -1,7 +1,20 @@
 $(document).ready(function() {
     $('.banner-slider').slick({
         dots: true,
-        arrows: true,
+        fade: true
+    });
+});
+$(document).ready(function() {
+    $('.share-content-slider').slick({
+        dots: true,
+        arrows: false,
+        fade: true
+    });
+});
+$(document).ready(function() {
+    $('.features-slider').slick({
+        dots: true,
+        arrows: false,
         fade: true
     });
 });
