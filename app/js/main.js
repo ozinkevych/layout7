7483e021c141311814926faaa071731762e2eb6c
ozinkevych <zinkevicsasa1@gmail.com>
2024-06-10 17:25:16 +0300

    task: 7.8 completed the compatibility block and the header, it remains to validate the footer

--- app/js/main.js
+++ app/js/main.js
@@ -18,3 +18,12 @@ $(document).ready(function() {
         fade: true
     });
 });
+const navbarToggler = document.querySelector('.navbar-toggler');
+
+const navbarTogglerIcon = navbarToggler.querySelector('.navbar-toggler-icon');
+
+navbarToggler.addEventListener('click', function() {
+    const isCollapsed = this.getAttribute('aria-expanded') === 'false';
+
+    navbarTogglerIcon.classList.toggle('open', isCollapsed);
+});
