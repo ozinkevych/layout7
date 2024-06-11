8d8a9afcee616e91243e56b70e5ad5239c950eec
ozinkevych <zinkevicsasa1@gmail.com>
2024-06-11 16:14:13 +0300

    task: 8 - Completed the footer and header block and fixed the bugs

--- app/js/main.js
+++ app/js/main.js
@@ -1,17 +1,18 @@
-$(document).ready(function() {
+$(document).ready(function () {
     $('.banner-slider').slick({
         dots: true,
+        arrows: false,
         fade: true
     });
 });
-$(document).ready(function() {
+$(document).ready(function () {
     $('.share-content-slider').slick({
         dots: true,
         arrows: false,
         fade: true
     });
 });
-$(document).ready(function() {
+$(document).ready(function () {
     $('.features-slider').slick({
         dots: true,
         arrows: false,
@@ -22,8 +23,21 @@ const navbarToggler = document.querySelector('.navbar-toggler');
 
 const navbarTogglerIcon = navbarToggler.querySelector('.navbar-toggler-icon');
 
-navbarToggler.addEventListener('click', function() {
+navbarToggler.addEventListener('click', function () {
     const isCollapsed = this.getAttribute('aria-expanded') === 'false';
 
     navbarTogglerIcon.classList.toggle('open', isCollapsed);
 });
+const form = document.getElementById('subscribeForm');
+
+form.addEventListener('submit', function(event) {
+    if (!form.checkValidity()) {
+        event.preventDefault();
+        event.stopPropagation();
+    }
+
+    form.classList.add('was-validated');
+});
+
+
+
