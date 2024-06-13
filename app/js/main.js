1c9e8d6418c5bf05017ef276cb5afe10903739f3
ozinkevych <zinkevicsasa1@gmail.com>
2024-06-13 13:34:21 +0300

    Corrected inaccuracies

--- app/js/main.js
+++ app/js/main.js
@@ -1,21 +1,25 @@
+//banner slider
 $(document).ready(function () {
     $('.banner-slider').slick({
         dots: true,
         arrows: false,
     });
 });
+//share instantly slider
 $(document).ready(function () {
     $('.share-content-slider').slick({
         dots: true,
         arrows: false,
     });
 });
+//features slider
 $(document).ready(function () {
     $('.features-slider').slick({
         dots: true,
         arrows: false,
     });
 });
+//burger menu
 const navbarToggler = document.querySelector('.navbar-toggler');
 
 const navbarTogglerIcon = navbarToggler.querySelector('.navbar-toggler-icon');
@@ -25,6 +29,7 @@ navbarToggler.addEventListener('click', function () {
 
     navbarTogglerIcon.classList.toggle('open', isCollapsed);
 });
+//form validation
 const form = document.getElementById('subscribeForm');
 
 form.addEventListener('submit', function(event) {
@@ -35,19 +40,22 @@ form.addEventListener('submit', function(event) {
 
     form.classList.add('was-validated');
 });
+// like
 document.addEventListener('DOMContentLoaded', function () {
     const heartIcons = document.querySelectorAll('.img-like');
 
     heartIcons.forEach(function (heartIcon) {
-        heartIcon.addEventListener('click', function () {
+        heartIcon.addEventListener('click', function handleClick() {
             const likeCount = parseInt(this.dataset.likeCount, 10);
             const likeCountElement = this.parentNode.querySelector('.like-count');
 
             const newLikeCount = likeCount + 1;
             likeCountElement.textContent = newLikeCount;
             this.dataset.likeCount = newLikeCount;
+
+            this.classList.add('fa-solid');
+            this.removeEventListener('click', handleClick);
         });
     });
 });
-
 
